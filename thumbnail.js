e349d9d4aed6f3b8b1dc7bb27df19be51a94102a
Allen <71183681+Allen494@users.noreply.github.com>
2021-07-24 17:18:41 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    //你的所有代码都应该写在这里
     alert("Hello JavaSrcipt");
+ 
+    targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+});
 });
