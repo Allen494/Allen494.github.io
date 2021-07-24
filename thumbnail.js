97c2af3d8a5fe0e429acf0cf1f342cfb94501da2
Allen <71183681+Allen494@users.noreply.github.com>
2021-07-24 17:55:54 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
    //你的所有代码都应该写在这里
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-      targetImage.classList.remove("small");
+       //targetImage.classList.remove("small");
+      if(targetImage.classList.contains("small")){
+         targetImage.classList.remove("small");
+      }else{
+         targetImage.classList.add("small");
+      }
     });  
 });
